f54a1bf4ee48f81b09dc8ba681374aaee0d1e571
Bent Schöne <bent.schoene@fh-bielefeld.de>
2026-08-27 23:29:18 +0200

    Add support for configurable CORS origins
    * Replace hardcoded CORS origin with a configurable ALLOWED_ORIGINS list
    * Remove FRONTEND_ORIGIN from required env vars as its no longer needed
    * Update local swagger server URL to new dev port
    * Update server startup log and Swagger production url to a newly added SERVER_URL env variable

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ app.use(compression());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.NODE_ENV === 'development' ? true : 'https://coderushapp.netlify.app',
+    origin: (process.env.ALLOWED_ORIGINS || 'http://localhost:3000').split(','),
     credentials: true,
   })
 );
